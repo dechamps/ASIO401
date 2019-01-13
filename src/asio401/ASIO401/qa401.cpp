78487e27e4aa3cd12499e9448c2966d104e4f2a4
Etienne Dechamps <etienne@edechamps.fr>
2019-01-13 15:20:39 +0000

    Validate that the USB device has all the endpoints we need.

--- src/asio401/ASIO401/qa401.cpp
+++ src/asio401/ASIO401/qa401.cpp
@@ -4,9 +4,23 @@
 
 #include "../ASIO401Util/windows_error.h"
 
+#include <dechamps_cpputil/string.h>
+
 #include <winusb.h>
 
+#include <set>
+
 namespace asio401 {
+
+	namespace {
+
+		constexpr UCHAR registerPipeId = 0x02;
+		constexpr UCHAR writePipeId = 0x04;
+		constexpr UCHAR readPipeId = 0x86;
+
+		const auto requiredPipeIds = { registerPipeId, writePipeId, readPipeId };
+
+	}
 
 	QA401::QA401(std::string_view devicePath) :
 		winUsb(WinUsbOpen(devicePath)) {
@@ -21,6 +35,7 @@ namespace asio401 {
 			throw std::runtime_error("No USB endpoints - did you run the QuantAsylum Analyzer app first to configure the hardware?");
 		}
 
+		std::set<UCHAR> missingPipeIds = requiredPipeIds;
 		for (UCHAR endpointIndex = 0; endpointIndex < usbInterfaceDescriptor.bNumEndpoints; ++endpointIndex) {
 			Log() << "Querying pipe #" << int(endpointIndex);
 			WINUSB_PIPE_INFORMATION pipeInformation = { 0 };
@@ -28,6 +43,10 @@ namespace asio401 {
 				throw std::runtime_error("Unable to query WinUSB pipe #" + std::to_string(int(endpointIndex)) + ": " + GetWindowsErrorString(GetLastError()));
 			}
 			Log() << "Pipe information: " << DescribeWinUsbPipeInformation(pipeInformation);
+			missingPipeIds.erase(pipeInformation.PipeId);
+		}
+		if (!missingPipeIds.empty()) {
+			throw std::runtime_error("Could not find WinUSB pipe IDs: " + ::dechamps_cpputil::Join(missingPipeIds, ", ", ::dechamps_cpputil::CharAsNumber()));
 		}
 	}
 
