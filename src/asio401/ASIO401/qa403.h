de5ce5f464b6bc1a99ac4a823fdd5a92f9b48aaf
Etienne Dechamps <etienne@edechamps.fr>
2024-04-28 21:36:31 +0100

    Certify QA403 constants

--- src/asio401/ASIO401/qa403.h
+++ src/asio401/ASIO401/qa403.h
@@ -11,10 +11,9 @@ namespace asio401 {
 	// This class implements the USB protocol described at https://github.com/QuantAsylum/QA40x_BareMetal
 	class QA403 {
 	public:
-		// TODO: these have been copy-pasted from QA401 and have not yet been verified empirically on the QA403.
 		static constexpr auto sampleSizeInBytes = 4;  // 32-bit big endian signed integer
 		static constexpr auto sampleEndianness = ::dechamps_cpputil::Endianness::LITTLE;
-		static constexpr auto hardwareQueueSizeInFrames = 1024;
+		static constexpr auto hardwareQueueSizeInFrames = 1024;  // Measured empirically
 		static constexpr auto inputChannelCount = 2;
 		static constexpr auto outputChannelCount = 2;
 		static constexpr auto sampleRate = 48000;  // TODO: support other sample rates
